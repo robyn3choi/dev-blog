a09bb6f585edb9d173ce07dcbac2a6de82ba5606
Robyn Choi <robyn3choi@gmail.com>
2020-06-21 16:38:09 -0700

    testing react-switch

--- src/components/ModeSwitch/index.jsx
+++ src/components/ModeSwitch/index.jsx
@@ -20,18 +20,16 @@ export default class ModeSwitch extends React.Component {
             return null
           }
           return (
-            <label>
-              <Switch
-                className="mode-switch"
-                aria-label="dark/light mode toggle"
-                checkedIcon={moonIcon}
-                uncheckedIcon={sunicon}
-                onChange={checked => toggleTheme(checked ? 'dark' : 'light')}
-                checked={theme === 'dark'}
-                offColor="#5a78bb"
-                onColor="#5a78bb"
-              />
-            </label>
+            <Switch
+              className="mode-switch"
+              aria-label="dark/light mode toggle"
+              checkedIcon={moonIcon}
+              uncheckedIcon={sunicon}
+              onChange={checked => toggleTheme(checked ? 'dark' : 'light')}
+              checked={theme === 'dark'}
+              offColor="#5a78bb"
+              onColor="#5a78bb"
+            />
           )
         }}
       </ThemeToggler>
